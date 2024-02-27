ae0ac65a9dd4fc2bcb4b00aa0d69ffc6458fa5b2
Karen Ann <workwithkaren@gmail.com>
2024-02-27 18:30:21 -0500

    Update sql

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -4,13 +4,15 @@ drop table if exists asset_type;
 create table asset_item
  (aid int(11) AUTO_INCREMENT,
   sku  varchar(8) NOT NULL,
-  note  varchar(32) DEFAULT NULL,,
-  primary key (aid)
+  akey varchar(4) NOT NULL,
+  note  varchar(32) DEFAULT NULL,
+  primary key (aid),
+  foreign key (akey) references asset_type (akey)
  );
 
 create table asset_type
  (
-  key  varchar(4) NOT NULL,
+  akey  varchar(4) NOT NULL,
   type  varchar(16) NOT NULL,
-  primary key (key)
+  primary key (akey)
  );
---
+++ sql/ins.sql
@@ -0,0 +1,2 @@
+INSERT INTO asset_type VALUES ('LAP', 'Laptop'), ('PER', 'Peripheral'), ('MON', 'Monitor');
+INSERT INTO asset_item VALUES ('000123', 'LAP', 'Good shape'), ('222123', 'MON', 'Scratched up'); 
