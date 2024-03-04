c8c11788121814973b5d090c7b773497c2a0dba8
Karen Ann <juggergnat@users.noreply.github.com>
2024-03-04 15:19:22 -0500

    Update ddl.sql asset_type needs to be first

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -5,6 +5,13 @@ use webdb;
 drop table if exists asset_item;
 drop table if exists asset_type;
 
+create table asset_type
+ (
+  akey  varchar(4) NOT NULL,
+  type  varchar(16) NOT NULL,
+  primary key (akey)
+ );
+
 create table asset_item
  (aid int(11) AUTO_INCREMENT,
   sku  varchar(8) NOT NULL,
@@ -12,11 +19,4 @@ create table asset_item
   note  varchar(32) DEFAULT NULL,
   primary key (aid),
   foreign key (akey) references asset_type (akey)
- );
-
-create table asset_type
- (
-  akey  varchar(4) NOT NULL,
-  type  varchar(16) NOT NULL,
-  primary key (akey)
  );
