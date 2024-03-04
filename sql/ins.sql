f250712300886b815e08fecb3de6ec0581e6be7d
Karen Ann <juggergnat@users.noreply.github.com>
2024-03-04 15:21:21 -0500

    Update ins.sql add item fields

--- sql/ins.sql
+++ sql/ins.sql
@@ -1,2 +1,2 @@
 INSERT INTO asset_type VALUES ('LAP', 'Laptop'), ('PER', 'Peripheral'), ('MON', 'Monitor');
-INSERT INTO asset_item VALUES ('000123', 'LAP', 'Good shape'), ('222123', 'MON', 'Scratched up'); 
+INSERT INTO asset_item (sku, akey, note) VALUES ('000123', 'LAP', 'Good shape'), ('222123', 'MON', 'Scratched up'); 
