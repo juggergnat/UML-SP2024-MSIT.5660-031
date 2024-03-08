472e69329ac05dc89fa912994ac0c5a927d6b04a
Karen Ann <workwithkaren@gmail.com>
2024-03-08 17:17:56 -0500

    Change type key and add person and image

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -8,7 +8,7 @@ drop table if exists asset_type;
 create table asset_type
  (
   akey  varchar(4) NOT NULL,
-  type  varchar(16) NOT NULL,
+  atype  varchar(16) NOT NULL,
   primary key (akey)
  );
 
@@ -16,7 +16,10 @@ create table asset_item
  (aid int(11) AUTO_INCREMENT,
   sku  varchar(8) NOT NULL,
   akey varchar(4) NOT NULL,
+  person varchar(40) DEFAULT NULL,
   note  varchar(32) DEFAULT NULL,
+  image varchar(256) DEFAULT NULL,
   primary key (aid),
   foreign key (akey) references asset_type (akey)
  );
+
